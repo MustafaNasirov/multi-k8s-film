784e27d81f69e1637d8aa61fd2c605f6d825c523
Mustafa Nasirov <musta1@macbook-pro.home>
2023-08-06 12:56:17 +0100

    fixed prop issue

--- client/src/components/MovieList/MovieList.js
+++ client/src/components/MovieList/MovieList.js
@@ -1,21 +1,20 @@
 import MovieCard from "../MovieCard/MovieCard";
 import "./MovieList.css";
 
-const MovieList = ({movies}) =>{
+const MovieList = ({ movieArr }) => { // Use movieArr instead of movies
 
     const movies = movieArr.map((movie) => {
         return (
-          <div key={movie.id} className='movie__card'>
-            <MovieCard
-              name={movie.title}
-              poster={movie.poster_path}
-            />
-          </div>
-        );
-      });
-      
-    
-    return <div className='movie__container'>{movies}</div>
+            <div key={movie.id} className='movie__card'>
+                <MovieCard
+                    name={movie.title}
+                    poster={movie.poster_path}
+                />
+            </div>
+        )
+    });
+
+    return <div className='movie__container'>{movies}</div>;
 }
 
 export default MovieList;
