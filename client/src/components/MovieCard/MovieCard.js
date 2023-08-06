509be9f48d7673e3dd2942ce2d9b9ef8d1c0a999
Mustafa Nasirov <musta1@macbook-pro.home>
2023-08-06 15:32:57 +0100

    updated poster path

--- client/src/components/MovieCard/MovieCard.js
+++ client/src/components/MovieCard/MovieCard.js
@@ -1,10 +1,10 @@
 import "./MovieCard.css";
 
-const MovieCard = ({ key, name, poster }) => { // instead of ({ id, title, poster_path })
+const MovieCard = ({ key, name, poster_path }) => { // instead of ({ id, title, poster_path })
   return (
     <div className="movie__card" key={key}>
       <p className="movie__title">{name}</p>
-      <img className="movie__poster" src={poster} alt="poster" />
+      <img className="movie__poster" src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt="poster" />
     </div> 
   );
 }
